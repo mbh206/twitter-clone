e300a728788bea3921de0f14b563ba3b00b3125b
Mark <75957685+mbh206@users.noreply.github.com>
2022-10-28 08:29:11 +0900

    Initial commit

---
+++ data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@FrittBot 💎`,
+        profilePic: `images/fritters-2.webp`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@FrittElon ✅`,
+        profilePic: `images/fritters-3.jpeg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@FritterCruise ✅`,
+                profilePic: `images/Fritter-4.jpeg`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@FrittNorris ✅`,
+                profilePic: `images/Fritter-5.jpeg`,
+                tweetText: `I went last year😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@FrittCoder12`,
+        profilePic: `images/Fritter-6.jpeg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@FrittHulk ☣️`,
+                profilePic: `images/Fritter-8.jpeg`,
+                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@YummyFritterer64`,
+                profilePic: `images/Fritter-9.jpeg`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
